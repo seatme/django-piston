aef1ea6aa110c1224495d398f140aa00f0ef74b8
Matt Behrens <askedrelic@gmail.com>
2014-09-11 17:17:49 -0700

    update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import os
 
 setup(
     name = "django-piston",
-    version = "0.2.3.sm8",
+    version = "0.2.3.sm9",
     url = 'http://bitbucket.org/jespern/django-piston/wiki/Home',
 	download_url = 'http://bitbucket.org/jespern/django-piston/downloads/',
     license = 'BSD',
